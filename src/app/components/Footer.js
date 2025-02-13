dc9c4d8f1711624e85b261caa8ba164e06299ad4
Sam Mecke <hyp3fox@Sams-MacBook-Pro.local>
2025-02-12 18:39:15 -0700

    Adding initial Character data and simple header and footer components

---
+++ src/app/components/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <footer className="bg-white">
+        <p className="text-black text-center">&copy; Shatterpoint</p>
+    </footer>
+  )
+}
+
+export default Footer
