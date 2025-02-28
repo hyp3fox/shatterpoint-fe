89ca13bea0144b18772a08aea8a1911de7770606
Sam Mecke <hyp3fox@Sams-MacBook-Pro.local>
2025-02-27 20:05:41 -0700

    Converted Search Results from List to Cards. Also converted Character tags to list.

--- src/app/components/CharactersList.js
+++ src/app/components/CharactersList.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import SearchBar from './SearchBar'
+import characters from '../../../characters.json'
+import CharactersCardList from './CharactersCardList'
 
 const CharactersList = () => {
   return (
     <div>
-        CharactersList
+        CharactersList:
         <SearchBar />
     </div>
   )
--- src/app/components/SearchBar.js
+++ src/app/components/SearchBar.js
@@ -13,6 +13,34 @@ const SearchBar = () => {
     return (
       <div className="p-4 max-w-md mx-auto">
         <input className="w-full p-2 border rounded-md" type="text" placeholder="Search..." value={query} onChange={(e) => setQuery(e.target.value)} />
+        
+        {filteredData.length > 0 ? (
+            filteredData.map(item => (
+              <div className="max-w-sm w-full lg:max-w-full lg:flex" key={item.id}>
+                <div className="border-r border-b border-l border-gray-400 lg:border-l-0 lg:border-t lg:border-gray-400 bg-white rounded-b lg:rounded-b-none lg:rounded-r p-4 flex flex-col justify-between leading-normal">
+                    <div className="mb-8">
+                    <img className="w-10 h-10 rounded-full mr-4" src="/img/jonathan.jpg" alt={item.era} />
+                    <div className="text-gray-900 font-bold text-xl mb-2">{item.name}</div>
+                    <p className="text-gray-700 text-base">
+                    {item.sp ? item.sp + "SP" + " " : item.pc +  "PC" + " "} {item.force} Force | {item.stamina} Stamina | {item.durability} Durability</p>
+                    </div>
+                    <div className="flex items-center">
+                    <div className="text-sm">
+                        <ul className="text-gray-900 leading-none tag-list">
+                          {item.keywords.map((keyword, index) =>  (
+                            <li className="tag" key={index}>{keyword}</li>
+                          ))}
+                        </ul>
+                    </div>
+                    </div>
+                </div>
+              </div>
+            ))
+          ) : (
+           <p>No results found</p>
+          )}
+        
+        
         <ul className="mt-2 border rounded-md p-2">
           {filteredData.length > 0 ? (
             filteredData.map(item => (
