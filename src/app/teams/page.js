b79fe83138371a7bd368a20475d6a78f031d2e7c
Sam Mecke <samantha.mecke92@gmail.com>
2025-04-08 15:24:02 -0700

    Added paragraph to describe what page does in team page.

--- src/app/teams/page.js
+++ src/app/teams/page.js
@@ -6,6 +6,7 @@ export default function Teams() {
     <div>
       <Header />
       <h1>This is the Teams page</h1>
+      <p>A list of added characters will display here based on selections made from the search area.</p>
       <Footer />
     </div>
   );
